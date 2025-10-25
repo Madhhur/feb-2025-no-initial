a6364062394df8691ce020be969fe5cff0352077
Madhhur <mm7599355000@gmail.com>
2025-10-25 17:57:21 +0530

    Add main test for calculator functions
    Added a simple test for add and subtract functions.

--- my_python_project/src/calculator.py
+++ my_python_project/src/calculator.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a + b
 
-
 def subtract(a, b):
     return a - b
+
+# Simple test when running as main
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 1 =", subtract(5, 1))
